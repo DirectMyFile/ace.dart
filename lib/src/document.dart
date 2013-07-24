97ecaa5a332f82937a1a09e34760d55518b240cb
rmsmith <ross.dart.lang@gmail.com>
2013-07-24 14:07:17 +0300

    Document : start wrapping a js.Proxy

--- lib/src/document.dart
+++ lib/src/document.dart
@@ -1,5 +1,21 @@
 part of ace;
 
 class Document {
-  // TODO(rms):
+  js.Proxy _proxy;
+  
+  int get length => _proxy.getLength();
+  
+  String
+    get value => _proxy.getValue();
+    set value(String text) => _proxy.setValue(text);
+  
+  Document._(js.Proxy proxy) : _proxy = js.retain(proxy);
+  
+  void dispose() {
+    assert(_proxy != null);
+    js.release(_proxy);
+    _proxy = null;
+  }
+  
+  String getLine(int row) => _proxy.getLine(row);
 }
