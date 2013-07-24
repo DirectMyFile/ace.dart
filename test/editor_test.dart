539632bc24df380f5e12d6005f065374f79d7e70
rmsmith <ross.dart.lang@gmail.com>
2013-07-24 17:17:37 +0300

    add `testBlurEditor` and `testFocusEditor`

--- test/editor_test.dart
+++ test/editor_test.dart
@@ -17,3 +17,23 @@ void testEditElementById() {
 void testEditElementBadIdThrows() {  
   final Editor a = edit("snarf");
 }
+
+@Test()
+void testBlurEditor() {
+  final Editor editor = edit('editor');  
+  editor.focus();
+  editor.onBlur.listen(expectAsync1((e) {
+    expect(e, equals(editor));
+  }));    
+  editor.blur();
+}
+
+@Test()
+void testFocusEditor() {
+  final Editor editor = edit('editor');  
+  editor.blur();
+  editor.onFocus.listen(expectAsync1((e) {
+    expect(e, equals(editor));
+  }));
+  editor.focus();  
+}
