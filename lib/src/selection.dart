55b52e8f7f2b43fdedcab72fd17231d9ef614bbe
rmsmith <ross.dart.lang@gmail.com>
2013-07-25 10:46:49 +0300

    Selection + some wrappers

--- lib/src/selection.dart
+++ lib/src/selection.dart
@@ -1,5 +1,29 @@
 part of ace;
 
 class Selection {
-  // TODO(rms):
+  js.Proxy _proxy;
+  
+  bool get isBackwards => _proxy.isBackwards();
+  bool get isEmpty => _proxy.isEmpty();
+  bool get isMultiLine => _proxy.isMultiLine();
+  
+  Selection._(js.Proxy proxy) : _proxy = js.retain(proxy);
+  
+  void dispose() {
+    assert(_proxy != null);
+    js.release(_proxy);
+    _proxy = null;
+  }
+  
+  void mergeOverlappingRanges() => _proxy.mergeOverlappingRanges();
+  void moveCursorBy(int rows, int columns) => 
+      _proxy.moveCursorBy(rows, columns);
+  void moveCursorDown() => _proxy.moveCursorDown();
+  void moveCursorFileEnd() => _proxy.moveCursorFileEnd();
+  void moveCursorFileStart() => _proxy.moveCursorFileStart();
+  void moveCursorLeft() => _proxy.moveCursorLeft();
+  void moveCursorLineEnd() => _proxy.moveCursorLineEnd();
+  void moveCursorLineStart() => _proxy.moveCursorLineStart();
+  void moveCursorLongWordLeft() => _proxy.moveCursorLongWordLeft();
+  void moveCursorLongWordRight() => _proxy.moveCursorLongWordRight();
 }
