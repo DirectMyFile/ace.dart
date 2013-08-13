7276459befddff85e809293ba069759a9e9c889b
rmsmith <ross.dart.lang@gmail.com>
2013-08-13 11:06:35 +0300

    move test libraries into 'test/src'

--- test/browser.dart
+++ test/browser.dart
@@ -2,9 +2,9 @@ library ace.test.browser;
 
 import 'package:bench/bench.dart' as bench;
 import 'package:unittest/html_enhanced_config.dart';
-import 'editor_test.dart';
-import 'point_test.dart';
-import 'range_test.dart';
+import 'src/editor_test.dart';
+import 'src/point_test.dart';
+import 'src/range_test.dart';
 
 main() {
   useHtmlEnhancedConfiguration();
