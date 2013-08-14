e8b2af93351c5f7c102a16601957d82a0fa19770
rmsmith <ross.dart.lang@gmail.com>
2013-08-14 14:58:39 +0300

    refactor sample text into its own library to be shared across test groups

--- test/src/document_test.dart
+++ test/src/document_test.dart
@@ -4,16 +4,7 @@ library ace.test.document;
 import 'package:ace/ace.dart';
 import 'package:bench/meta.dart';
 import 'package:unittest/unittest.dart';
-
-final String sampleText =
-'''
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-culpa qui officia deserunt mollit anim id est laborum.
-''';
+import 'sample_text.dart';
 
 Document document;
 // TODO(rms): figure out how to invoke the js `new Document(text)` ctor...
@@ -22,46 +13,26 @@ setup() => document = createEditSession(sampleText, 'ace/mode/dart').document;
 
 @Test()
 void testGetLength() {
-  expect(document.length, equals(7));
+  expect(document.length, equals(6));
 }
 
 @Test()
 void testGetAllLines() {
   final lines = document.allLines;
-  expect(lines.length, equals(7));
-  expect(lines[0], equals('Lorem ipsum dolor sit amet, consectetur '
-                          'adipisicing elit, sed do eiusmod tempor'));
-  expect(lines[1], equals('incididunt ut labore et dolore magna aliqua. Ut '
-                          'enim ad minim veniam, quis'));
-  expect(lines[2], equals('nostrud exercitation ullamco laboris nisi ut '
-                          'aliquip ex ea commodo consequat. '));
-  expect(lines[3], equals('Duis aute irure dolor in reprehenderit in '
-                          'voluptate velit esse cillum dolore eu '));
-  expect(lines[4], equals('fugiat nulla pariatur. Excepteur sint occaecat '
-                          'cupidatat non proident, sunt in '));
-  expect(lines[5], equals('culpa qui officia deserunt mollit anim id est '
-                          'laborum.'));
-  expect(lines[6], equals(''));
+  expect(lines.length, equals(6));
+  expect(lines[0], equals(sampleTextLine0));
+  expect(lines[1], equals(sampleTextLine1));
+  expect(lines[2], equals(sampleTextLine2));
+  expect(lines[3], equals(sampleTextLine3));
+  expect(lines[4], equals(sampleTextLine4));
+  expect(lines[5], equals(sampleTextLine5));
 }
 
 @Test()
 void testGetLine() {
-  expect(document.getLine(0), 
-      equals('Lorem ipsum dolor sit amet, consectetur '
-             'adipisicing elit, sed do eiusmod tempor'));
-  expect(document.getLine(1), 
-      equals('incididunt ut labore et dolore magna aliqua. Ut '
-             'enim ad minim veniam, quis'));
-  expect(document.getLine(2), 
-      equals('nostrud exercitation ullamco laboris nisi ut '
-             'aliquip ex ea commodo consequat. '));
-  expect(document.getLine(3), 
-      equals('Duis aute irure dolor in reprehenderit in '
-             'voluptate velit esse cillum dolore eu '));
-  expect(document.getLine(4), 
-      equals('fugiat nulla pariatur. Excepteur sint occaecat '
-             'cupidatat non proident, sunt in '));
-  expect(document.getLine(5), 
-      equals('culpa qui officia deserunt mollit anim id est laborum.'));
-  expect(document.getLine(6), equals(''));
+  expect(document.getLine(0), equals(sampleTextLine0));
+  expect(document.getLine(1), equals(sampleTextLine1));
+  expect(document.getLine(2), equals(sampleTextLine2));
+  expect(document.getLine(3), equals(sampleTextLine3));
+  expect(document.getLine(4), equals(sampleTextLine4));
 }
---
+++ test/src/sample_text.dart
@@ -0,0 +1,26 @@
+library ace.test.sample_text;
+
+const String sampleTextLine0 = 
+'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod ';
+
+const String sampleTextLine1 =
+'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,';
+
+const String sampleTextLine2 =
+' quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo ';
+
+const String sampleTextLine3 =
+'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse';
+
+const String sampleTextLine4 =
+' cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat ';
+
+const String sampleTextLine5 =
+'non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+final String sampleText = sampleTextLine0 + '\n' + 
+                          sampleTextLine1 + '\n' + 
+                          sampleTextLine2 + '\n' +
+                          sampleTextLine3 + '\n' +
+                          sampleTextLine4 + '\n' +
+                          sampleTextLine5;
