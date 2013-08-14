fb99bd6a62655aba6ffa8e36062c256c2c11ad51
rmsmith <ross.dart.lang@gmail.com>
2013-08-14 14:59:33 +0300

    edit_session_test now uses shared sample text

--- test/src/edit_session_test.dart
+++ test/src/edit_session_test.dart
@@ -4,16 +4,7 @@ library ace.test.edit_session;
 import 'package:ace/ace.dart';
 import 'package:bench/meta.dart';
 import 'package:unittest/unittest.dart';
-
-final String sampleText =
-'''
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-culpa qui officia deserunt mollit anim id est laborum.
-''';
+import 'sample_text.dart';
 
 @Test()
 void testCreateEditSession() {  
