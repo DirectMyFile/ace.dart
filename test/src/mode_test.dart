026b6406ca560aa86b196d33baf68038ded5ea1d
rmsmith <ross.dart.lang@gmail.com>
2013-08-28 21:01:46 +0300

    mode_test : increasing coverage of different modes / extensions
    TODO: more coverage

--- test/src/mode_test.dart
+++ test/src/mode_test.dart
@@ -5,16 +5,33 @@ import 'package:ace/ace.dart';
 import 'package:bench/bench.dart';
 import 'package:unittest/unittest.dart';
 
+final _noop = (_){};
+
 @Test()
 void testCreateModeFromModePath() {
-  final Mode mode = new Mode('ace/mode/text');
-  expect(mode, isNotNull);  
-  mode.onHasProxy.then(expectAsync1((_) {}));
+  final verifyMode = (String modePath) {
+    expect(new Mode(modePath)..onHasProxy.then(expectAsync1(_noop)), isNotNull);
+  };
+  verifyMode('ace/mode/batchfile');
+  verifyMode('ace/mode/c_cpp');
+  verifyMode('ace/mode/csharp');
+  verifyMode('ace/mode/css');
+  verifyMode('ace/mode/dart');
+  verifyMode('ace/mode/javascript');
+  verifyMode('ace/mode/text');  
 }
 
 @Test()
 void testCreateModeFromFilePath() {
-  final Mode mode = new Mode.forFile('path/to/file.js');
-  expect(mode, isNotNull);
-  mode.onHasProxy.then(expectAsync1((_) {})); 
+  final verifyMode = (String filePath) {
+    expect(new Mode.forFile(filePath)..onHasProxy.then(expectAsync1(_noop)), 
+           isNotNull);
+  };
+  verifyMode('some/script.bat');
+  verifyMode('vm/allocator.cpp');
+  verifyMode('vm/allocator.h');
+  verifyMode('vm/gc.cs');
+  verifyMode('twit/boot.css');
+  verifyMode('ftw.dart');
+  verifyMode('some/legacy.js');
 }
