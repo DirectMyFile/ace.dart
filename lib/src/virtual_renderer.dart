8de019b0ca5d2dfe7d05706953511342cd66e5e4
rmsmith <ross.dart.lang@gmail.com>
2013-09-21 18:11:16 +0300

    VirtualRenderer : add `updateFull` with doc

--- lib/src/virtual_renderer.dart
+++ lib/src/virtual_renderer.dart
@@ -7,4 +7,10 @@ class VirtualRenderer extends _HasProxy {
   int get firstVisibleRow => _proxy.getFirstVisibleRow();
   
   VirtualRenderer._(js.Proxy proxy) : super(proxy);
+  
+  /// Updates all of the layers, for all the rows.
+  /// 
+  /// By default this schedules an asynchronous update.  If [force] is `true`
+  /// then the update is performed immediately.
+  void updateFull({bool force: false}) => _proxy.updateFull(force);
 }
