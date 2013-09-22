98d957c09818030f1d9624c6171a26c11f1c3f9e
rmsmith <ross.dart.lang@gmail.com>
2013-09-22 20:01:47 +0300

    _HasProxy : no need for the dartdoc on the privates

--- lib/src/_.dart
+++ lib/src/_.dart
@@ -5,15 +5,8 @@ get _context => js.context;
 abstract class _HasProxy {
   var _proxy;
   
-  /// A future that completes when this object first obtains an underlying
-  /// javascript proxy object.
-  /// 
-  /// Some of the objects in Ace require an asynchronous computation to fully
-  /// initialize the underlying proxy.  This future provides a way to observe
-  /// when that computation has completed.
   final Future _onHasProxy;
   
-  /// Returns `true` if this object has an underlying javascript proxy object.
   bool get _hasProxy => _proxy != null;
       
   _HasProxy.async(Future<js.Proxy> proxyFuture) 
