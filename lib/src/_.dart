a673a377ff65b9a5b41377e3f9dffee006a055de
rmsmith <ross.dart.lang@gmail.com>
2013-09-22 15:46:32 +0300

    _HasProxy : doc for the public interface

--- lib/src/_.dart
+++ lib/src/_.dart
@@ -5,8 +5,15 @@ get _context => js.context;
 abstract class _HasProxy {
   var _proxy;
   
+  /// A future that completes when this object first obtains an underlying
+  /// javascript proxy object.
+  /// 
+  /// Some of the objects in Ace require an asynchronous computation to fully
+  /// initialize the underlying proxy.  This future provides a way to observe
+  /// when that computations has completed.
   final Future onHasProxy;
   
+  /// Returns `true` if this object has an underlying javascript proxy object.
   bool get hasProxy => _proxy != null;
       
   _HasProxy.async(Future<js.Proxy> proxyFuture) 
@@ -18,11 +25,13 @@ abstract class _HasProxy {
       : _proxy = js.retain(proxy)
       , onHasProxy = new Future.value();
   
+  /// Dispose of and release the underlying javascript proxy object, if any.
   void dispose() {
-    assert(hasProxy);
-    _onDispose();
-    js.release(_proxy);
-    _proxy = null;
+    if (hasProxy) {
+      _onDispose();
+      js.release(_proxy);
+      _proxy = null;
+    }
   }
   
   void _onDispose() {}
