9afe531125c7df47cc10f83edf9c749bb138290d
rmsmith <ross.dart.lang@gmail.com>
2013-10-07 12:14:56 +0300

    vm/Document : more work-in-progress porting

--- lib/src/vm/document.dart
+++ lib/src/vm/document.dart
@@ -1,7 +1,8 @@
 part of ace;
 
 class _Document implements Document {
-  
+  static final _newLineRegExp = new RegExp(r"/^.*?(\r\n|\r|\n)/m");
+  String _autoNewLine;
   final List<String> _lines = new List<String>();
   
   final _onChange = new StreamController<Delta>.broadcast();
@@ -52,31 +53,64 @@ class _Document implements Document {
   
   Anchor createAnchor(int row, int column) => throw new UnimplementedError();
   
+  void _detectNewLine(String text) {
+    var match = _newLineRegExp.firstMatch(text);
+    _autoNewLine = match != null ? match : "\n";
+  }
+  
   List<String> getAllLines() => _lines.toList(growable: false);
   
   List<String> getLines(int firstRow, int lastRow) => 
       _lines.getRange(firstRow, lastRow + 1).toList(growable: false);
   
-  String getLine(int row) => throw new UnimplementedError();
+  String getLine(int row) {
+    if (row < 0 || row >= length) return "";
+    return _lines[row];
+  }
   
-  Point insert(Point position, String text) => throw new UnimplementedError();
+  Point insert(Point position, String text) {
+    if (text == null || text.length == 0) {
+      return position;
+    }
+    position = _clipPosition(position);    
+    if (length <= 1) {
+      _detectNewLine(text);
+    }      
+    final lines = _split(text);
+    final firstLine = lines.removeAt(0);
+    final lastLine = lines.length == 0 ? null : lines.removeLast();
+    position = insertInLine(position, firstLine);
+    if (lastLine != null) {
+      position = insertNewLine(position);
+      position = _insertLines(position.row, lines);
+      position = insertInLine(position, lastLine != null ? lastLine : "");
+    }
+    return position;
+  }
   
   Point insertInLine(Point position, String text) {
-    if (text.length == 0) return position;    
-    var line = _lines[position.row];
-    if (line == null) line = "";
-    _lines[position.row] = 
+    if (text.length == 0) return position;        
+    final line = (position.row >= length) ? "" : _lines[position.row];    
+    final lineText = 
         line.substring(0, position.column) + 
         text + 
         line.substring(position.column);
+    if (position.row < length) {
+      _lines[position.row] = lineText;
+    } else {
+      _lines.insert(position.row, lineText);
+    }
     final end = new Point(position.row, position.column + text.length);
     final delta = new InsertTextDelta._(
-        new Range.fromPoints(position, end), text);    
+        new Range.fromPoints(position, end), text);
     _onChange.add(delta);
     return end;
   }
   
   Point insertLines(int row, Iterable<String> lines) => 
+      throw new UnimplementedError();
+  
+  Point _insertLines(int row, Iterable<String> lines) => 
       throw new UnimplementedError();
   
   Point insertNewLine(Point position) => throw new UnimplementedError();
@@ -111,11 +145,6 @@ class _Document implements Document {
   }
   
   List<String> removeLines(int startRow, int endRow) {
-    // TODO(rms): ace.js does not assert on the arguments, but rather returns a
-    // call to `remove`; that won't fly with Dart's type checker and seems way
-    // too magical, so for now I've implemented asserts.  If we start to hit
-    // these asserts then we should investigate what funky code paths rely on
-    // calls to this method with out of range arguments.
     assert(startRow >= 0);
     assert(endRow < length);
     return this._removeLines(startRow, endRow);
@@ -138,4 +167,8 @@ class _Document implements Document {
   Point replace(Range range, String text) => throw new UnimplementedError();
   
   void revertDeltas(Iterable<Delta> deltas) => throw new UnimplementedError();
+  
+  List<String> _split(String text) {
+    return text.split(r"/\r\n|\r|\n/");
+  }
 }
