81a1b19325be8490937a755bc90fa653963194c5
rmsmith <dev@futureperfect.info>
2014-02-28 17:14:33 +0200

    ace_test.js : invert the order in the loader script to try first the `ace_test.dart.js`
    Chrome 33 seems to be okay with Dart scripts so we no longer get an
    error ... this changes to trying to load first the js; it won't be there
    until we `pub build` so we'll get an error and fall back to the dart
    script.  It means we get an error in development but it can safely be
    ignored ...

--- test/ace_test.js
+++ test/ace_test.js
@@ -1,11 +1,11 @@
 // We do not check user agent because we run javascript in Dartium content_shell
 var script = document.createElement('script');
-script.type = 'application/dart';
-script.src = 'ace_test.dart';  
+script.type = 'text/javascript';
+script.src = 'ace_test.dart.js';
 script.addEventListener('error', function() {
   script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.src = 'ace_test.dart.js'; 
+  script.type = 'application/dart';
+  script.src = 'ace_test.dart';
   document.body.appendChild(script);
 });
 document.body.appendChild(script);
