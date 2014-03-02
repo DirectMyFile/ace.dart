0f78dfe03e5c9c7cbf5a5ce3d98adc039f471b59
rmsmith <dev@futureperfect.info>
2014-03-02 17:18:47 +0200

    drone.sh : cleanup file

--- drone.sh
+++ drone.sh
@@ -16,22 +16,13 @@ sudo start xvfb
 ./content_shell/content_shell --dump-render-tree test/ace_test.html
 
 # Build test application javascript
-#pub build test
+pub build test
 
 # TODO: is there a Chromium content_shell we can run here instead of Dartium?
 # Run tests as javascript w/ content_shell
-#./content_shell/content_shell --dump-render-tree build/test/ace_test.html
+./content_shell/content_shell --dump-render-tree build/test/ace_test.html
 
-# TODO: dartdoc was removed from the Dart SDK
 # Generate API docs and push to gh-pages
-# dartdoc --package-root packages --include-lib ace,ace.proxy lib/ace.dart lib/proxy.dart
-# git checkout gh-pages
-# cd docs/
-# cp -r . ..
-# cd ../
-
-# TODO: docgen work-in-progress; the polyfill javascripts are in out/packages/
-# but so are _many_ dart files so we need to selectively copy the js 
 docgen --compile --package-root packages --no-include-sdk --no-include-dependent-packages lib/ace.dart lib/proxy.dart
 git checkout gh-pages
 rm -r packages/
