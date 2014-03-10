cc0ac63cbde162179db203af8d0d292cefd1f706
rmsmith <dev@futureperfect.info>
2014-03-10 12:43:25 +0200

    Command : add dartdoc and scroll / select 'enums'

--- lib/src/command.dart
+++ lib/src/command.dart
@@ -2,24 +2,52 @@ part of ace;
 
 abstract class Command extends Disposable {
   
+  static const SCROLL_NONE            = null;
+  static const SCROLL_CENTER          = 'center';
+  static const SCROLL_ANIMATE         = 'animate';
+  static const SCROLL_CURSOR          = 'cursor';
+  static const SCROLL_SELECTION_PART  = 'selectionPart'; 
+  static const List<String> SCROLL_ACTIONS = const [ 
+    SCROLL_NONE, SCROLL_CENTER, SCROLL_ANIMATE, SCROLL_CURSOR, 
+    SCROLL_SELECTION_PART
+  ];
+  
+  static const SELECT_NONE            = null;
+  static const SELECT_FOR_EACH        = 'forEach';
+  static const SELECT_FOR_EACH_LINE   = 'forEachLine';
+  static const SELECT_SINGLE          = 'single';
+  static const List<String> SELECT_ACTIONS = const [ 
+    SELECT_NONE, SELECT_FOR_EACH, SELECT_FOR_EACH_LINE, SELECT_SINGLE
+  ];
+  
+  /// The unique name for this command.
   String get name;
   
   BindKey get bindKey;
  
   Function get exec;
   
+  /// Whether or not this command applies to read-only [Editor] instances.
   bool get readOnly;
   
-  // one of `null`, `center`, `animate`, `cursor` or `selectionPart`
+  /// The scroll action for this command.
+  /// 
+  /// The scroll action is one of the values in [SCROLL_ACTIONS].
   String get scrollIntoView;
   
-  // one of `null`, `forEach`, `forEachLine` or `single`
+  /// The select action for this command.
+  /// 
+  /// The select action is one of the values in [SELECT_ACTIONS].
   String get multiSelectAction;
   
   factory Command(String name, BindKey bindKey, exec(Editor), 
-      {bool readOnly: false, String scrollIntoView, String multiSelectAction})
-      => implementation.createCommand(name, bindKey, exec, readOnly: readOnly, 
-          scrollIntoView: scrollIntoView, multiSelectAction: multiSelectAction);
+      {bool readOnly: false, String scrollIntoView: SCROLL_NONE, 
+      String multiSelectAction: SELECT_NONE}) {
+    assert(SCROLL_ACTIONS.contains(scrollIntoView));
+    assert(SELECT_ACTIONS.contains(multiSelectAction));
+    return implementation.createCommand(name, bindKey, exec, readOnly: readOnly, 
+        scrollIntoView: scrollIntoView, multiSelectAction: multiSelectAction);
+  }
 }
 
 class BindKey {
