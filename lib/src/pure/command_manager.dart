6180af568779fb371c0e869fd9dc15fdf46c33b4
rmsmith <dev@futureperfect.info>
2014-03-11 21:08:17 +0200

    _CommandManager :  implement `_bindKey` and stub `_parseKeys`

--- lib/src/pure/command_manager.dart
+++ lib/src/pure/command_manager.dart
@@ -3,6 +3,9 @@ part of ace.pure;
 class _CommandManager extends Disposable implements CommandManager {
   
   final String _platform;
+  final Map<String, Command> _commands = new Map<String, Command>();
+  final Map<int, Map<String, Command>> _commandKeyBinding = 
+      new Map<int, Map<String, Command>>();
   
   _CommandManager(String platform, Iterable<Command> commands)
       : _platform = (platform == null) ? _htmlPlatform : platform {
@@ -17,5 +20,21 @@ class _CommandManager extends Disposable implements CommandManager {
   
   List<Command> getCommands() => throw new UnimplementedError();
   
-  void removeCommand(String commandName) => throw new UnimplementedError();  
+  void removeCommand(String commandName) => throw new UnimplementedError();
+  
+  void _bindKey(String key, Command command) {    
+    key.split('|').forEach((String keyPart) {
+      final binding = _parseKeys(keyPart);
+      final int hashId = binding[1];
+      if (_commandKeyBinding[hashId] == null) {
+        _commandKeyBinding[hashId] = new Map<String, Command>();
+      }
+      _commandKeyBinding[hashId][binding[0]] = command;      
+    });
+  }
+  
+  List _parseKeys(String keys) {
+    // TODO:
+    return null;
+  }
 }
