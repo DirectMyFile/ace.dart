c230d28d3e077860b409792f85214a55784374f3
rmsmith <dev@futureperfect.info>
2014-05-02 11:10:41 +0300

    language_tools : small code formatting

--- lib/src/proxy/language_tools.dart
+++ lib/src/proxy/language_tools.dart
@@ -6,34 +6,34 @@ class _CodeCompleterReverseProxy extends _HasReverseProxy
   final Function getCompletions;
   
   _CodeCompleterReverseProxy(this.getCompletions) {
-      _proxy['getCompletions'] = (editor, session, pos, prefix, 
-          js.JsFunction callback) {
-        final _editor = editor == null ? null 
-            : new _EditorProxy._(editor, listen: false);
-        final _session = session == null ? null 
-            : new _EditSessionProxy._(session, listen: false);        
-        getCompletions(_editor, _session, pos, prefix)
-        ..then((results) {
-          callback.apply([null, _jsArray(results.map(_jsCompletion))]);
-        })
-        ..catchError((e) {
-          // TODO: should we pass the error (string) as the first argument?
-          callback.apply([null, _jsArray([])]);
-        })
-        ..whenComplete(() {
-          if (_editor != null) {
-            _editor.dispose();  
-          }
-          if (_session != null) {
-            _session.dispose();
-          }
-        });
-      };
-    }
+    _proxy['getCompletions'] = (editor, session, pos, prefix, 
+        js.JsFunction callback) {
+      final _editor = editor == null ? null 
+          : new _EditorProxy._(editor, listen: false);
+      final _session = session == null ? null 
+          : new _EditSessionProxy._(session, listen: false);        
+      getCompletions(_editor, _session, pos, prefix)
+      ..then((results) {
+        callback.apply([null, _jsArray(results.map(_jsCompletion))]);
+      })
+      ..catchError((e) {
+        // TODO: should we pass the error (string) as the first argument?
+        callback.apply([null, _jsArray([])]);
+      })
+      ..whenComplete(() {
+        if (_editor != null) {
+          _editor.dispose();  
+        }
+        if (_session != null) {
+          _session.dispose();
+        }
+      });
+    };
+  }
     
-    void _onDispose() {
-      _proxy.deleteProperty('getCompletions');
-    }
+  void _onDispose() {
+    _proxy.deleteProperty('getCompletions');
+  }
 }
 
 class _LanguageToolsProxy extends HasProxy implements LanguageTools {
