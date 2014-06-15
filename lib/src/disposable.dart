b31e11e3ece2411736b89a654b72759f3b7e73e4
rmsmith <dev@futureperfect.info>
2014-06-15 18:25:05 +0300

    disposable : fix type `help` -> `held`

--- lib/src/disposable.dart
+++ lib/src/disposable.dart
@@ -3,7 +3,7 @@ part of ace;
 abstract class Disposable {
   /// Dispose of any resources held by this object.
   /// 
-  /// This method will release any javascript proxy objects help by this 
+  /// This method will release any javascript proxy objects held by this 
   /// object.  It will also close any streams exposed by the object.  It is an
   /// error to call any methods or access any fields of this object after this
   /// method has been called.
