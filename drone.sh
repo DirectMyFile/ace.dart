43d1abe13c93f57daa300902ed86e0b902cc9d99
rmsmith <dev@futureperfect.info>
2014-07-11 16:11:31 +0300

    drone.sh : build example and deploy to gh-pages

--- drone.sh
+++ drone.sh
@@ -20,3 +20,18 @@ pub build test
 
 # Run tests as javascript w/ content_shell
 ./content_shell/content_shell --dump-render-tree build/test/ace_test.html
+
+# Build the examples
+pub build example
+
+# Deploy the examples to gh-pages
+git checkout gh-pages
+mkdir -p examples
+cd build/example/
+cp -r . ../../examples
+cd ../../
+
+git add -A
+git diff-index --quiet HEAD || git commit -m"auto commit from drone"
+git remote set-url origin git@github.com:rmsmith/ace.dart.git
+git push origin gh-pages
